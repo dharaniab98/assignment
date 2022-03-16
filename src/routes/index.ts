110822920b325fb09e05bb3ffcd6802ed7c9edcf
medadharanikumar <dharani.meda@netenrich.com>
2022-03-16 11:18:16 +0530

    feature log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import teamRouter from './team.routes';
 
 export default class Router {
   constructor(app: Application) {
+    // health check done
     app.route('/health').get((req: Request, res: Response) => {
       res.send({ status: 'success', message: 'working fine' });
     });
